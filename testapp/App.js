01759f67bb1140489acca90fe7f593b2dd3f144c
TonyTwofold <ajglindegren@gmail.com>
2021-03-27 18:32:38 +0100

    created section list

--- testapp/App.js
+++ testapp/App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import {StyleSheet, Text, TextInput, View, Image, Button} from 'react-native';
+import {StyleSheet, Text, TextInput, View, Image, Button, SectionList} from 'react-native';
 
-const wordTranslator = () => {
+const styles = StyleSheet.create( {
+    container: {
+        flex: 1,
+        paddingTop: 40,
+        backgroundColor: '#f98',
+    },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: 'bold',
+        backgroundColor: 'white',
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
 
-    const [text, setText] = useState('');
-    return {
-      
-    };
+const SectionListBasics = () => {
+    return (
+        <View style={styles.container}>
+            <SectionList
+                sections={[
+                    {title: 'Monday', data: ['Bench press', 'Triceps', 'Shoulders']},
+                    {title: 'Tuesday', data: ['Squats', 'Deadlift', 'Leg extention']},
+                ]}
+                renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+                renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                keyExtractor={(item, index) => index}
+            />
+
+        </View>
+    );
 }
+
+export default SectionListBasics;
